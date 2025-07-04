9194e4cff9c17694a8f7b74eb0508cb0ac9c74f8
lms0806 <godmlzkf1@naver.com>
2025-07-05 01:55:41 +0900

    add : 2285

---
+++ 1 ~ 5000/2285.rs
@@ -0,0 +1,78 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input.trim().to_string()
+    }
+
+    pub fn line_eof(&mut self) -> Option<String> {
+        let mut input = String::new();
+        let bytes_read = self.reader.read_line(&mut input).ok()?;
+        if bytes_read == 0 {
+            return None; // EOF
+        }
+        Some(input.trim().to_string())
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<usize>();
+
+    let mut v = vec![(0, 0); n];
+
+    let mut num = 0;
+    for i in 0..n {
+        v[i] = (scan.token::<i64>(), scan.token::<i64>());
+        num += v[i].1;
+    }
+
+    v.sort();
+
+    let (mut sum, mut answer, target) = (0, 0, (num + 1) / 2);
+    for (a, b) in v {
+        sum += b;
+
+        if sum >= target {
+            answer = a;
+            break;
+        }
+    }
+    write!(out, "{}", answer).unwrap();
+}
