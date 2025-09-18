d961983c616cc677c88fb2fac7fdb9bfd096d6ac
lms0806 <godmlzkf1@naver.com>
2025-09-18 21:56:43 +0900

    add : 9/18

---
+++ 30001 ~ 35000/34291.rs
@@ -0,0 +1,75 @@
+use std::io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line_eof(&mut self) -> Option<String> {
+        let mut input = String::new();
+        let bytes_read = self.reader.read_line(&mut input).expect("Failed read");
+
+        if bytes_read == 0 {
+            None
+        } else {
+            Some(input)
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let (n, mut c) = (scan.token::<usize>(), scan.token::<i64>());
+    let mut arr = (0..n)
+        .map(|_| (scan.token::<usize>(), scan.token::<i64>()))
+        .collect::<Vec<_>>();
+    let mut check = vec![false; 24];
+
+    arr.sort_unstable_by(|a, b| a.1.cmp(&b.1));
+
+    let mut answer = 0;
+    for (a, b) in arr {
+        if !check[a] && c - b >= 0 {
+            answer += 1;
+            check[a] = true;
+            c -= b;
+        }
+    }
+    write!(out, "{}", answer).unwrap();
+}
---
+++ 30001 ~ 35000/34364.rs
@@ -0,0 +1,52 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let _ = scan.token::<usize>();
+
+    let mut idx = 1;
+    for ch in scan.token::<String>().chars() {
+        write!(
+            out,
+            "{}",
+            ((ch as i64 - 'A' as i64 + idx).rem_euclid(26) + 'A' as i64) as u8 as char
+        )
+        .unwrap();
+        idx <<= 1;
+    }
+}
